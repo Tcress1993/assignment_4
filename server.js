f033073430f52944f1da30682c4bf445e1dadcac
Tcress1993 <cresswellt54@gmail.com>
2025-04-09 06:47:53 -0600

    fix errors

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ var cors = require('cors');
 var User = require('./Users');
 var Movie = require('./Movies');
 var Review = require('./Reviews');
-
+require('dotenv').config();
 var app = express();
 app.use(cors());
 app.use(bodyParser.json());

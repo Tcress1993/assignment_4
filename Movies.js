a47ba58b78ea85e5c73d011c73565b648fd34451
Tcress1993 <cresswellt54@gmail.com>
2025-04-17 05:33:16 -0600

    add image

--- Movies.js
+++ Movies.js
@@ -14,6 +14,7 @@ var MovieSchema = new Schema({
     releaseDate: {type: Number, min: [1900, 'Must be greater than 1899'], max: [2100, "Must be less than 2100"]},
     genre: {type: String, enum: ['Action', 'Adventure', 'Comedy', 'Drama', 'Fantasy', 'Horror', 'Mystery', 'Thriller', 'Western', 'Science Fiction'],},
     actors: [{actorName: String, characterName: String}],
+    imageUrl: String
 });
 
 // return the model
